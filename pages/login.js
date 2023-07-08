18ae447a9b76b399d6077f1b4829e50a85d11298
unknown <matuuzg@gmail.com>
2023-07-08 17:10:56 -0500

    validacion y alamcenamiento en LS

---
+++ pages/login.js
@@ -0,0 +1,71 @@
+const emailField = document.querySelector("[name=email]");
+const passwordField = document.querySelector("[name=password]");
+
+
+// LOCAL STORAGE ----
+const savedEmail = localStorage.getItem("email");
+const savedPassword = localStorage.getItem("password");
+
+emailField.value = savedEmail ? savedEmail : "";
+passwordField.value = savedPassword ? savedPassword : "";
+
+emailField.addEventListener("input", () => {
+    localStorage.setItem("email", emailField.value);
+  });
+
+  passwordField.addEventListener("input", () => {
+    localStorage.setItem("password", passwordField.value);
+  });
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    event.preventDefault(); 
+
+
+    window.location.href = "../index.html";
+  });
+
+// VALIDACIÓN DE INPUTS ----
+
+const setErrors = (message, field, isError = true) => {
+    if (isError) {
+      field.classList.add("invalid");
+      field.nextElementSibling.classList.add("error");
+      field.nextElementSibling.innerText = message;
+    } else {
+      field.classList.remove("invalid");
+      field.nextElementSibling.classList.remove("error");
+      field.nextElementSibling.innerText = "";
+    }
+  }
+
+  const validateEmptyField = (message, e) => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    if (fieldValue.trim().length === 0) {
+      setErrors(message, field);
+    } else {
+      setErrors("", field, false);
+    }
+  }
+  
+  const validateEmailFormat = e => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+      setErrors("Please enter a valid email", field);
+    } else {
+      setErrors("", field, false);
+    }
+  }
+
+
+  const init = () => {
+
+   
+    emailField.addEventListener("blur", (e) => validateEmptyField("Please provide an email", e));
+    passwordField.addEventListener("blur", (e) => validateEmptyField("Write your password", e));
+    emailField.addEventListener("input", validateEmailFormat);
+};
+
+init();
--- pages/register.js
+++ pages/register.js
@@ -1,73 +1,76 @@
-const registerForm = document.getElementById("register-form");
-const nameInput = document.getElementById("name");
-const lastNameInput = document.getElementById("last-name");
+const userNameField = document.querySelector("[name=username]");
+const passwordField = document.querySelector("[name=password]");
+const emailField = document.querySelector("[name=email]");
 
-
-
-// VALIDACIÓN DE INPUTS ----
-
-const isEmpty = (input) => {
-    return !input.value.trim().length;
-};
-
-const betweenCharacters = (input, min, max) => {
-    return input.value.length >= min && input.value.length <= max;
-};
-
-const showErrorToUser = (input, message) => {
-const inputContainer= input.parentElement;
-inputContainer.classList.remove("success");
-inputContainer.classList.add("error");
-const error = inputContainer.querySelector("small");
-error.style.display = "block";
-error.textContent = message;
-
+const setErrors = (message, field, isError = true) => {
+  if (isError) {
+    field.classList.add("invalid");
+    field.nextElementSibling.classList.add("error");
+    field.nextElementSibling.innerText = message;
+  } else {
+    field.classList.remove("invalid");
+    field.nextElementSibling.classList.remove("error");
+    field.nextElementSibling.innerText = "";
+  }
 }
 
-const showSuccessToUser = (input) => {
-    const inputContainer= input.parentElement;
-    inputContainer.classList.remove("error");
-    inputContainer.classList.add("success");
-    const error = inputContainer.querySelector("small");
-    error.style.display = "block";
-    error.textContent = "";
-};
-
-
-const checkTextInput = (input) => {
-    let valid = false;
-    const minCharacters = 4;
-    const maxCharacters = 20;
-
-if (isEmpty(input)) {
-    showErrorToUser(input, "Please complete this field");
-    return;
-};
-
-
-
-if(!betweenCharacters(input, minCharacters, maxCharacters)){
-    showErrorToUser(input, 
-        `Must write ${minCharacters}' and ${maxCharacters} characters`);
-        return;
-
+const validateEmptyField = (message, e) => {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  if (fieldValue.trim().length === 0) {
+    setErrors(message, field);
+  } else {
+    setErrors("", field, false);
+  }
 }
 
-showSuccessToUser(input);
-valid = true
-return valid;
+const validateEmailFormat = e => {
+  const field = e.target;
+  const fieldValue = e.target.value;
+  const regex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+  if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+    setErrors("Please enter a valid email", field);
+  } else {
+    setErrors("", field, false);
+  }
+}
 
+const submitButton = document.querySelector("#submit-button");
+
+const saveDataToLocalStorage = () => {
+  const userName = userNameField.value;
+  const password = passwordField.value;
+  const email = emailField.value;
+
+  
+  validateEmptyField("Add your Full Name", { target: userNameField });
+  validateEmptyField("Write your password", { target: passwordField });
+  validateEmptyField("Please provide an email", { target: emailField });
+  validateEmailFormat({ target: emailField });
+
+  
+  const isValid =
+    !userNameField.classList.contains("invalid") &&
+    !passwordField.classList.contains("invalid") &&
+    !emailField.classList.contains("invalid");
+
+  if (isValid) {
+    // Guardar los datos en el localStorage
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("password", password);
+    localStorage.setItem("email", email);
+
+    window.location.href = "/pages/login.html";
+  }
 };
-
-
-
-// VALIDACIÓN Y REGISTRO DE DATOS ----
-
-
 
 const init = () => {
-registerForm.addEventListener("submit", validateForm);
-nameInput.addEventListener("input", () => checkTextInput(nameInput));
+  userNameField.addEventListener("blur", (e) => validateEmptyField("Add your Full Name", e));
+  passwordField.addEventListener("blur", (e) => validateEmptyField("Write your password", e));
+  emailField.addEventListener("blur", (e) => validateEmptyField("Please provide an email", e));
+  emailField.addEventListener("input", validateEmailFormat);
+  submitButton.addEventListener("click", saveDataToLocalStorage);
 };
 
 init();
+
